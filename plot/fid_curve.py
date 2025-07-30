ec5b99332cc150a20121860056a6e95f7be92f48
Chen <49557610+2019ChenGong@users.noreply.github.com>
2025-07-30 16:08:40 -0400

    Update fid_curve.py
    Signed-off-by: Chen <49557610+2019ChenGong@users.noreply.github.com>

--- plot/fid_curve.py
+++ plot/fid_curve.py
@@ -32,10 +32,10 @@ matplotlib.rcParams.update({'font.size': fontsize, 'font.weight': 'normal'})
 fig = plt.figure(figsize=(20, 4))
 axes = fig.subplots(1, 5)
 
-log_files = ['/p/fzv6enresearch/DPImageBench/exp/dpgan/mnist_28_eps1.0trainval-2024-10-22-17-08-37/stdout.txt',
-            '/p/fzv6enresearch/DPImageBench/exp/dpdm/mnist_28_eps1.0trainval-2024-10-23-00-58-27/stdout.txt',
-            '/p/fzv6enresearch/DPImageBench/exp/dp-feta/mnist_28_eps1.0val_central_mean-2025-03-19-07-56-01/stdout.txt',
-            '/p/fzv6enresearch/PE-Refine/exp/dp-feta2/mnist_28_eps1.0val_merfeps10.0_fetasigma20_merf0.25-2025-05-25-23-57-45/stdout.txt',]
+log_files = ['/p//DPImageBench/exp/dpgan/mnist_28_eps1.0trainval-2024-10-22-17-08-37/stdout.txt',
+            '/p//DPImageBench/exp/dpdm/mnist_28_eps1.0trainval-2024-10-23-00-58-27/stdout.txt',
+            '/p//DPImageBench/exp/dp-feta/mnist_28_eps1.0val_central_mean-2025-03-19-07-56-01/stdout.txt',
+            '/p//PE-Refine/exp/dp-feta2/mnist_28_eps1.0val_merfeps10.0_fetasigma20_merf0.25-2025-05-25-23-57-45/stdout.txt',]
 fid_list = []
 for log in log_files:
     with open(log, 'r') as f:
@@ -64,10 +64,10 @@ for i, fids in enumerate(fid_list):
     #             small_x = x[j]
 print(x[-1], small_x, small_x/x[-1])
 
-log_files = ['/p/fzv6enresearch/DPImageBench/exp/dpgan/fmnist_28_eps1.0trainval-2024-10-22-15-52-27/stdout.txt',
-            '/p/fzv6enresearch/DPImageBench/exp/dpdm/fmnist_28_eps1.0trainval-2024-10-23-23-32-54/stdout.txt',
-            '/p/fzv6enresearch/DPImageBench/exp/dp-feta/fmnist_28_eps1.0val_central_mean-2025-03-20-23-02-01/stdout.txt',
-            '/p/fzv6enresearch/PE-Refine/exp/dp-feta2/fmnist_28_eps1.0val_merfeps1.0_fetasigma20_merf0.25-2025-05-26-16-18-46/stdout.txt',
+log_files = ['/p//DPImageBench/exp/dpgan/fmnist_28_eps1.0trainval-2024-10-22-15-52-27/stdout.txt',
+            '/p//DPImageBench/exp/dpdm/fmnist_28_eps1.0trainval-2024-10-23-23-32-54/stdout.txt',
+            '/p//DPImageBench/exp/dp-feta/fmnist_28_eps1.0val_central_mean-2025-03-20-23-02-01/stdout.txt',
+            '/p//PE-Refine/exp/dp-feta2/fmnist_28_eps1.0val_merfeps1.0_fetasigma20_merf0.25-2025-05-26-16-18-46/stdout.txt',
             ]
 fid_list = []
 for log in log_files:
@@ -99,10 +99,10 @@ for i, fids in enumerate(fid_list):
     #             small_x = x[j]
 print(x[-1], small_x, small_x/x[-1])
 
-log_files = ['/p/fzv6enresearch/DPImageBench/exp/dpgan/cifar10_32_eps1.0trainval-2024-10-22-21-23-15/stdout.txt',
-            '/p/fzv6enresearch/DPImageBench/exp/dpdm/cifar10_32_eps1.0trainval-2024-10-23-14-01-14/stdout.txt',
-            '/p/fzv6enresearch/DPImageBench/exp/dp-feta/cifar10_32_eps1.0val_central_mean-2025-03-23-07-48-09/stdout.txt',
-            '/p/fzv6enresearch/PE-Refine/exp/dp-feta2/cifar10_32_eps1.0val_merfeps1.0_fetasigma15_merf0.2-2025-05-29-13-52-20/stdout.txt',
+log_files = ['/p//DPImageBench/exp/dpgan/cifar10_32_eps1.0trainval-2024-10-22-21-23-15/stdout.txt',
+            '/p//DPImageBench/exp/dpdm/cifar10_32_eps1.0trainval-2024-10-23-14-01-14/stdout.txt',
+            '/p//DPImageBench/exp/dp-feta/cifar10_32_eps1.0val_central_mean-2025-03-23-07-48-09/stdout.txt',
+            '/p//PE-Refine/exp/dp-feta2/cifar10_32_eps1.0val_merfeps1.0_fetasigma15_merf0.2-2025-05-29-13-52-20/stdout.txt',
             ]
 fid_list = []
 for log in log_files:
@@ -133,10 +133,10 @@ for i, fids in enumerate(fid_list):
                 small_x = x[j]
 print(x[-1], small_x, small_x/x[-1])
 
-log_files = ['/p/fzv6enresearch/DPImageBench/exp/dpgan/celeba_male_32_eps1.0trainval-2024-10-22-23-05-09/stdout.txt',
-            '/p/fzv6enresearch/DPImageBench/exp/dpdm/celeba_male_32_eps1.0trainval-2024-10-24-00-50-04/stdout.txt',
-            '/p/fzv6enresearch/DPImageBench/exp/dp-feta/celeba_male_32_eps1.0sen_central_mean-2025-03-18-02-23-26/stdout.txt',
-            '/p/fzv6enresearch/PE-Refine/exp/dp-feta2/celeba_male_32_eps1.0val_merfeps10.0_fetasigma15_merf0.15-2025-05-29-09-37-02/stdout.txt',
+log_files = ['/p//DPImageBench/exp/dpgan/celeba_male_32_eps1.0trainval-2024-10-22-23-05-09/stdout.txt',
+            '/p//DPImageBench/exp/dpdm/celeba_male_32_eps1.0trainval-2024-10-24-00-50-04/stdout.txt',
+            '/p//DPImageBench/exp/dp-feta/celeba_male_32_eps1.0sen_central_mean-2025-03-18-02-23-26/stdout.txt',
+            '/p//PE-Refine/exp/dp-feta2/celeba_male_32_eps1.0val_merfeps10.0_fetasigma15_merf0.15-2025-05-29-09-37-02/stdout.txt',
             ]
 fid_list = []
 for log in log_files:
@@ -172,10 +172,10 @@ for i, fids in enumerate(fid_list):
                 small_x = x[j]
 print(x[-1], small_x, small_x/x[-1])
 
-log_files = ['/p/fzv6enresearch/DPImageBench/exp/dpgan/camelyon_32_eps1.0trainval-2024-10-22-21-20-01/stdout.txt',
-            '/p/fzv6enresearch/DPImageBench/exp/dpdm/camelyon_32_eps1.0trainval-2024-10-24-08-46-55/stdout.txt',
-            '/p/fzv6enresearch/DPImageBench/exp/dp-feta/camelyon_32_eps1.0val_central_mean-2025-03-21-05-02-55/stdout.txt',
-            '/p/fzv6enresearch/PE-Refine/exp/dp-feta2/camelyon_32_eps1.0val_merfeps1.0_fetasigma10_merf0.15-2025-05-27-20-59-08/stdout.txt',
+log_files = ['/p//DPImageBench/exp/dpgan/camelyon_32_eps1.0trainval-2024-10-22-21-20-01/stdout.txt',
+            '/p//DPImageBench/exp/dpdm/camelyon_32_eps1.0trainval-2024-10-24-08-46-55/stdout.txt',
+            '/p//DPImageBench/exp/dp-feta/camelyon_32_eps1.0val_central_mean-2025-03-21-05-02-55/stdout.txt',
+            '/p//PE-Refine/exp/dp-feta2/camelyon_32_eps1.0val_merfeps1.0_fetasigma10_merf0.15-2025-05-27-20-59-08/stdout.txt',
             ]
 fid_list = []
 for log in log_files:
