b7e57ae5ebfc05221b35b39a20099aed32c831cc
Chen Gong <ChenG_abc@outlook.com>
2025-08-10 21:02:23 -0400

    doc: edit results demo of cifar10 1.0

--- scripts/script-feta-pro.sh
+++ scripts/script-feta-pro.sh
@@ -15,3 +15,6 @@ python run.py setup.n_gpus_per_node=3 setup.master_port=6662 eval.mode=val -m DP
 
 # RQ3, change the privacy allocation plans
 python run.py setup.n_gpus_per_node=4 --method DP-FETA-Pro --data_name mnist_28 -e 1.0 eval.mode=val train.sigma_freq=26.6 train.sigma_time=20
+
+# Eval
+CUDA_VISIBLE_DEVICES=0 python eval.py --method DP-FETA-Pro --data_name cifar10_32 --epsilon 10.0 --exp_path exp/dp-feta-pro/cifar10_32_eps10.0default_500_0.005-2025-08-09-01-47-59
