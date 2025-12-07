7b2fcfc3f99e15ac5537a6e343e21861cca108be
Chen Gong <cheng_abc@outlook.com>
2025-12-07 13:31:31 -0500

    edit: data_preparation

--- data_preparation.sh
+++ data_preparation.sh
@@ -2,6 +2,6 @@ mkdir dataset;
 cd dataset;
 mkdir celeba; cd celeba; gdown https://drive.google.com/uc?id=1nISkV4NyL9Ntd9E_A8dU2YpvO6Pcw8lx; unzip celeba.zip; cd ..
 mkdir camelyon; cd camelyon; gdown https://drive.google.com/uc?id=1cqjFYIAkERAWapbI6rmf_F9x9IBfz8Gl; unzip camelyon_32.zip; cd ..
-mkdir imagenet; cd imagenet; gdown https://drive.google.com/uc?id=1SFvDfBWmG30xTjFJ0v9Em5avRVL6yqAh; unzip imagenet_32.zip; cd ..
+mkdir imagenet; cd imagenet; gdown https://drive.google.com/uc?id=1SFvDfBWmG30xTjFJ0v9Em5avRVL6yqAh; unzip imagenet_32.zip; mv imagenet32 imagenet_32; cd ..
 cd ..; cd data;
 python preprocess_dataset.py --data_name mnist fmnist cifar10 celeba camelyon imagenet; cd ..
